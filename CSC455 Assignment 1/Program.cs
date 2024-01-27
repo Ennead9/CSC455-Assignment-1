4652ad3b6fa5dee266f92baff13aa7b08f09f631
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2024-01-27 08:54:28 -0600

    Add random integer feature

--- CSC455 Assignment 1/Program.cs
+++ CSC455 Assignment 1/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace CSC455_Assignment_1
 {
@@ -10,19 +10,22 @@ namespace CSC455_Assignment_1
     {
         static void Main(string[] args)
         {
-            bool keepRunning = true;
+            Random rnd = new Random();  // Prep for random numbers
+            bool keepRunning = true;    // Bool for exiting menu loop
+
+            // Welcome message
+            Console.WriteLine("Welcome! Please select an option (or press 'Q' to quit)");
 
             while (keepRunning)
             {
                 // Display options/console menu
-                Console.WriteLine("Welcome! Please select an option (or press 'Q' to quit)");
                 Console.WriteLine("1) Random Integer");
                 Console.WriteLine("2) Today's Date");
                 Console.WriteLine("3) Dino Names");
                 Console.WriteLine("4) Random String Action");
                 Console.WriteLine("Q) Quit\n");
 
-                Console.Write("Your option: ");
+                Console.Write("Please select an option: ");
 
                 // Read from user
                 string userInput = Console.ReadLine();
@@ -33,6 +36,15 @@ namespace CSC455_Assignment_1
 
                     // Random int
                     case "1":
+                        int randomInt = rnd.Next(1, 11); // Generate random int between 1 and 10
+                        Console.WriteLine("Drum roll please...");
+                        for(int i = 0; i < 4; i++)
+                        {
+                            Console.Write(". ");
+                            Thread.Sleep(600);
+                        }
+                        Console.WriteLine($"\nYour lucky number is: {randomInt}\n");
+
                         break;
                     
                     // Today's Date
