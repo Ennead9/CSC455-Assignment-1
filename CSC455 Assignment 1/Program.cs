0f9e2f9426151c87207d52f0b2222e66d49ebfcc
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2024-01-27 09:38:21 -0600

    Add dinosaur names list feature Generates and orders list by name thus far

--- CSC455 Assignment 1/Program.cs
+++ CSC455 Assignment 1/Program.cs
@@ -6,6 +6,38 @@ using System.Threading;
 
 namespace CSC455_Assignment_1
 {
+    public class Dino
+    {
+        public string Name {  get; set; }
+        public string Period { get; set; }
+        public string Diet { get; set; }
+
+        // Constructor
+        public Dino(string name, string period, string diet)
+        {
+            Name = name;
+            Period = period;
+            Diet = diet;
+        }
+
+        public static List<Dino> GenerateDinos()
+        {
+            return new List<Dino>
+            {
+                new Dino("Placerias", "Triassic", "Herbivore"),
+                new Dino("Eoraptor", "Triassic", "Omnivore"),
+                new Dino("Proganchelys", "Triassic", "Herbivore"),
+                new Dino("Cryptoclidus", "Jurassic", "Carnivore"),
+                new Dino("Vulcanodon", "Jurassic", "Herbivore"),
+                new Dino("Dimorphodon", "Jurassic", "Carnivore"),
+                new Dino("Pterodactyl", "Jurassic", "Carnivore"),
+                new Dino("Triceratops", "Cretaceous", "Herbivore"),
+                new Dino("Utahraptor", "Cretaceous", "Carnivore"),
+                new Dino("Tyrannosaurus", "Cretaceous", "Carnivore"),
+                
+            };
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -23,36 +55,50 @@ namespace CSC455_Assignment_1
                 Console.WriteLine("2) Today's Date");
                 Console.WriteLine("3) Dino Names");
                 Console.WriteLine("4) Random String Action");
-                Console.WriteLine("Q) Quit\n");
-
+                Console.WriteLine("Q) Quit");
                 Console.Write("Please select an option: ");
 
                 // Read from user
                 string userInput = Console.ReadLine();
+                Console.WriteLine();
 
                 // Handle input
                 switch (userInput)
                 {
-
-                    // Random int
+                    // Random integer from 1 to 10
                     case "1":
-                        int randomInt = rnd.Next(1, 11); // Generate random int between 1 and 10
+                        int randomInt = rnd.Next(1, 11); // Generate int
                         Console.WriteLine("Drum roll please...");
+                        // Drum roll for fun
                         for(int i = 0; i < 4; i++)
                         {
                             Console.Write(". ");
-                            Thread.Sleep(600);
+                            Thread.Sleep(400);
                         }
                         Console.WriteLine($"\nYour lucky number is: {randomInt}\n");
-
                         break;
                     
-                    // Today's Date
+                    // Today's Date in Short Date String format
                     case "2":
+                        DateTime date = DateTime.Now;
+                        var shortDate = date.ToShortDateString();
+                        Console.WriteLine($"Today's date is: {shortDate}\n");
+
                         break;
                     
                     // Dino Names
                     case "3":
+                        List<Dino> dinoList = Dino.GenerateDinos();
+                        
+                        // Order by name using LINQ
+                        dinoList = dinoList.OrderBy(a => a.Name).ToList();
+
+                        // Print ordered list out to console
+                        foreach (var dino in dinoList)
+                            Console.WriteLine($"{dino.Name}");
+
+                        Console.WriteLine("\n");
+
                         break;
 
                     // Random String Action
