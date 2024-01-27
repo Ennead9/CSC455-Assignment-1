a6234d9001f9156d1dc0ff9271ba6b339bfefa6e
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2024-01-27 08:35:51 -0600

    Begin to set up overall program structure, Add switch cases for menu options

--- CSC455 Assignment 1/Program.cs
+++ CSC455 Assignment 1/Program.cs
@@ -10,6 +10,51 @@ namespace CSC455_Assignment_1
     {
         static void Main(string[] args)
         {
+            bool keepRunning = true;
+
+            while (keepRunning)
+            {
+                // Display options/console menu
+                Console.WriteLine("Welcome! Please select an option (or press 'Q' to quit)");
+                Console.WriteLine("1) Random Integer");
+                Console.WriteLine("2) Today's Date");
+                Console.WriteLine("3) Dino Names");
+                Console.WriteLine("4) Random String Action");
+                Console.WriteLine("Q) Quit\n");
+
+                Console.Write("Your option: ");
+
+                // Read from user
+                string userInput = Console.ReadLine();
+
+                // Handle input
+                switch (userInput)
+                {
+
+                    // Random int
+                    case "1":
+                        break;
+                    
+                    // Today's Date
+                    case "2":
+                        break;
+                    
+                    // Dino Names
+                    case "3":
+                        break;
+
+                    // Random String Action
+                    case "4":
+                        break;
+
+                    // Quit
+                    case "Q":
+                        Console.WriteLine("Goodbye!");
+                        keepRunning = false;
+                        break;
+                }
+            }
+
         }
     }
 }
