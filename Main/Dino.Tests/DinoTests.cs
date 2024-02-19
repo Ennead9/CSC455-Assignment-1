b699919f45455dc642e7c4b1f83b8b9b95558f3b
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2024-02-18 20:20:55 -0600

    Test for generate random int

--- Main/Dino.Tests/DinoTests.cs
+++ Main/Dino.Tests/DinoTests.cs
@@ -1,23 +1,25 @@
-
+using FluentAssertions;
 
 namespace Dino.Tests
 {
     [TestClass]
     public class RandomNumberTests
     {
+        [DataTestMethod]
+        [DataRow(1, 11)]
+        [DataRow(2, 6)]
+        [DataRow(34, 45)]
         [TestMethod]
-        public void GenerateRandomIntTest()
+        public void GenerateRandomIntTest(int min, int max)
         {
             // Arrange
-            int min = 1;
-            int max = 11;
             var rnd = new Random();
 
             // Act
             int result = rnd.Next(min, max);
 
             // Assert
-            result.Should().Be(min);
+            result.Should().BeGreaterThanOrEqualTo(min).And.BeLessThanOrEqualTo(max);
         }
     }
 }
